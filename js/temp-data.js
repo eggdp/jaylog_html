1a6214a0a1e3df986069ffa96c28c7bbf8bc4be4
jaybon1 <ponggas2@gmail.com>
2022-11-03 01:53:34 +0900

    createDate, updateDate timestamp로 변경

---
+++ js/temp-data.js
@@ -0,0 +1,105 @@
+const cardDataList = [
+  {
+    thumbnail:
+      "https://velog.velcdn.com/images/lhr4884/post/8f8aba67-0e4d-4a7b-a73f-d09a8b69177d/image.png",
+    title: `Rxjs 한번 배워보실래요?`,
+    summary: `진짜 좋다고 생각해요. 배우기 어려워서 그렇지. 웬만한 개발자들은 배워야 할 이유를 납득하는 것 부터 어려울걸요? xJs를 대체 할 만한게 있을까요?" 크루: "솔직히 비동기나 시간을 다루는 데에는 Rxjs를 대체 할 만한게 없긴 하죠. 진짜 좋다고 생각해요. 배우기 어려워서 그렇지. 웬만한 개발자들은 배워야 할 이유를 납득하는 것 부터 어려울걸요?`,
+    updateDate: "2021-10-10",
+    writerProfileImage:
+      "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png",
+    writer: "jaybon",
+    likeCount: 10,
+  },
+  {
+    thumbnail:
+      "https://velog.velcdn.com/images/bepyan/post/f6db6cf8-f069-4a37-808c-4963049a584e/image.gif",
+    title: `Rxjs 한번 배워보실래요?`,
+    summary: `크루: "솔직히 비동기나 시간을 다루는 데에는 Rxjs를 대체 할 만한게 없긴 하죠. 진짜 좋다고 생각해요. 배우기 어려워서 그렇지. 웬만한 개발자들은 배워야 할 이유를 납득하는 것 부터 어려울걸요? xJs를 대체 할 만한게 있을까요?" 크루: "솔직히 비동기나 시간을 다루는 데에는 Rxjs를 대체 할 만한게 없긴 하죠. 진짜 좋다고 생각해요. 배우기 어려워서 그렇지. 웬만한 개발자들은 배워야 할 이유를 납득하는 것 부터 어려울걸요?`,
+    updateDate: "2021-10-10",
+    writerProfileImage:
+      "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png",
+    writer: "jaybon",
+    likeCount: 10,
+  },
+  {
+    thumbnail:
+      "https://velog.velcdn.com/images/purplew/post/85f27743-0b86-42f8-86cf-c1fe3d4c0f31/image.png",
+    title: `Rxjs 한번 배워보실래요?`,
+    summary: `배우기 어려워서 그렇지. 웬만한 개발자들은 배`,
+    updateDate: "2021-10-10",
+    writerProfileImage:
+      "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png",
+    writer: "jaybon",
+    likeCount: 10,
+  },
+  {
+    thumbnail:
+      "https://velog.velcdn.com/images/velopert/post/043d71d9-5a66-4795-b960-ba7ff9384947/image.png",
+    title: `Rxjs 한번 배워보실래요?`,
+    summary: `TxJs를 대체 할 만한게 있을까요?" 크루: "솔직히 비동기나 시간을 다루는 데에는 Rxjs를 대체 할 만한게 없긴 하죠. 진짜 좋다고 생각해요. 배우기 어려워서 그렇지. 웬만한 개발자들은 배`,
+    updateDate: "2021-10-10",
+    writerProfileImage:
+      "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png",
+    writer: "jaybon",
+    likeCount: 10,
+  },
+  {
+    thumbnail:
+      "https://velog.velcdn.com/images/lhr4884/post/8f8aba67-0e4d-4a7b-a73f-d09a8b69177d/image.png",
+    title: `Rxjs 한번 배워보실래요?`,
+    summary: ` 웬만한 개발자들은 배워야 할 이유를 납득하는 것 부터 어려울걸요? xJs를 대체 할 만한게 있을까요?" 크루: "솔직히 비동기나 시간을 다루는 데에는 Rxjs를 대체 할 만한게 없긴 하죠. 진짜 좋다고 생각해요. 배우기 어려워서 그렇지. 웬만한 개발자들은 배워야 할 이유를 납득하는 것 부터 어려울걸요?`,
+    updateDate: "2021-10-10",
+    writerProfileImage:
+      "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png",
+    writer: "jaybon",
+    likeCount: 10,
+  },
+  {
+    thumbnail:
+      "https://velog.velcdn.com/images/bepyan/post/f6db6cf8-f069-4a37-808c-4963049a584e/image.gif",
+    title: `Rxjs 한번 배워보실래요?`,
+    summary: `TxJs를 대체 할 만한게 있을까요?" 크루: "솔직히 비동기나 시간을 다루는 데에는 Rxjs를 대체 할 만한게 없긴 하죠. 진짜 좋다고 생각해요. 배우기 어려워서 그렇지. 웬만한 개발자들은 배워야 할 이유를 납득하는 것 부터 어려울걸요? xJs를 대체 할 만한게 있을까요?" 크루: "솔직히 비동기나 시간을 다루는 데에는 Rxjs를 대체 할 만한게 없긴 하죠. 진짜 좋다고 생각해요. 배우기 어려워서 그렇지. 웬만한 개발자들은 배워야 할 이유를 납득하는 것 부터 어려울걸요?`,
+    updateDate: "2021-10-10",
+    writerProfileImage:
+      "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png",
+    writer: "jaybon",
+    likeCount: 10,
+  },
+  {
+    thumbnail:
+      "https://velog.velcdn.com/images/purplew/post/85f27743-0b86-42f8-86cf-c1fe3d4c0f31/image.png",
+    title: `Rxjs 한번 배워보실래요?`,
+    summary: `TxJs를 대체 할 만한게 있을까요?" 크루: "솔직히 비동기나 시간을 다루는 데에는 Rxjs를 대체 할 만한게 없긴 하죠. 진짜 좋다고 생각해요. 배우기 어려워서 그렇지. 웬만한 개발자들은 배`,
+    updateDate: "2021-10-10",
+    writerProfileImage:
+      "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png",
+    writer: "jaybon",
+    likeCount: 10,
+  },
+];
+
+const postData = {
+  content: `
+# 실수 / 고정소수점 / 부동소수점
+
+# 문제
+고정소수점, 부동소수점 모두 프로그래밍 계산시
+0.1 또는 1.2 등 대부분 실수는 나타낼 수 없고, 계산하면 오차값이 생긴다.
+
+![](https://velog.velcdn.com/images/jaybon/post/4e14af44-0d67-4dd6-950f-74e6719c31a4/image.png)
+
+# 해결법
+2진법의 근본적인 문제이기 때문에 속시원한 해결방법은 없다고 봐도 된다.
+그래서...
+
+## 반올림 처리
+반올림 처리이니 주의한다.
+
+## 라이브러리 사용
+### decimal
+정확한 계산을 해 준다.
+(물론 무한 순환되는 실수는 어쩔 수 없다.)
+
+문자열로 나오니 Number등으로 형변환 해주자.
+    `,
+};
